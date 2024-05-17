3d48c74ba3fb6443f19bee73ba78dd62d2a540d6
lima.rodrigues <limarodrigues.silva@eb.mil.br>
2024-05-17 13:05:11 -0300

    iniciando projeto com Electron + React + TypeScript

---
+++ electron/electron.ts
@@ -0,0 +1,38 @@
+import path = require("path");
+
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, "index.html"));
+
+  //win.loadURL('http://localhost:3000');
+
+  win.once('ready', () => {
+    win.webContents.openDevTools();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ electron/preload.ts
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+})
+
+//this is justs for TypeScript error
+export {};
