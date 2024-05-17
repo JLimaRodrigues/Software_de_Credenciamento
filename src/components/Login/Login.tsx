753f0c2c438cf45d72f4edb96e50aea9807b0230
lima.rodrigues <limarodrigues.silva@eb.mil.br>
2024-05-17 16:44:18 -0300

    modificações para o backend

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Login: React.FC = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const handleLogin = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/api/pessoal');
+            console.log(response.data);
+        } catch (error) {
+            console.warn('Erro: ', error);
+        }
        console.log(`Login: ${username}, Senha: ${password}`);
       };
 
