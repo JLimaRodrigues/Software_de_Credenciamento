f11dc2dee3a89c65338754cba3ecdd293fc38af0
JLimaRodrigues <jeffersonlima147@hotmail.com>
2024-05-23 22:32:59 -0300

    atualizando o pagination

--- src/components/Users/TableUsers/TableFooter/index.tsx
+++ src/components/Users/TableUsers/TableFooter/index.tsx
@@ -1,28 +1,50 @@
 import React, { useEffect } from "react";
 import './styles.css';
 
-const TableFooter: React.FC<{range: any[], setPage: (value: number) => void, page: number, slice: any[]}> = ({ range, setPage, page, slice}) => {
+const TableFooter: React.FC<{
+  range: number[],
+  setPage: (value: number) => void,
+  page: number,
+}> = ({ range, setPage, page }) => {
 
-    useEffect(() => {
-        if (slice.length < 1 && page !== 1) {
-          setPage(page - 1);
-        }
-      }, [slice, page, setPage]);
-      return (
-        <div className="table-footer">
-        {range.map((el, index) => (
-            <button
+  useEffect(() => {
+    if (range.length < 1 && page !== 1) {
+      setPage(page - 1);
+    }
+  }, [range, page, setPage]);
+
+  const handleFirstPage = () => setPage(1);
+  const handleLastPage = () => setPage(range.length);
+  const handlePreviousPage = () => setPage(page > 1 ? page - 1 : 1);
+  const handleNextPage = () => setPage(page < range.length ? page + 1 : range.length);
+
+  return (
+    <div className="table-footer">
+      <button onClick={handleFirstPage} disabled={page === 1} className="button-table">
+        {"<<"}
+      </button>
+      <button onClick={handlePreviousPage} disabled={page === 1} className="button-table">
+        {"<"}
+      </button>
+      {range.map((el, index) => (
+        (el === page || el === page - 1 || el === page + 1) && (
+          <button
             key={index}
-            className={` button-table " ${
-                page === el ? 'table-active-button' : 'table-inactive-button'
-            }`}
+            className={`button-table ${page === el ? 'table-active-button' : 'table-inactive-button'}`}
             onClick={() => setPage(el)}
-            >
+          >
             {el}
-            </button>
-        ))}
-        </div>
-      );
+          </button>
+        )
+      ))}
+      <button onClick={handleNextPage} disabled={page === range.length} className="button-table">
+        {">"}
+      </button>
+      <button onClick={handleLastPage} disabled={page === range.length} className="button-table">
+        {">>"}
+      </button>
+    </div>
+  );
 }
 
 export default TableFooter;
--- src/components/Users/TableUsers/index.tsx
+++ src/components/Users/TableUsers/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Pessoa, db } from "../../../backend/db";
-import { deletePessoa, getPessoa, getPessoas } from "../../../backend/dataService";
+import { deletePessoa, getPessoa } from "../../../backend/dataService";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 
@@ -18,64 +18,64 @@ const TableUsers: React.FC = () => {
 
   const data = pessoasQuery || [];
   const { slice, range } = useTable(data, page, rowsPerPage);
-  
 
   const UpdatePessoa = async (id: number): Promise<void> => {
-      try {
-          const response = await getPessoa(id);
-          console.log('Pessoa: ', response)
-      } catch (error) {
-          console.log('Erro: ', error)
-      }
-  }
+    try {
+      const response = await getPessoa(id);
+      console.log('Pessoa: ', response);
+    } catch (error) {
+      console.log('Erro: ', error);
+    }
+  };
 
   const DeletePessoa = async (id: number): Promise<void> => {
-      try {
-          await deletePessoa(id);
-      } catch (error) {
-          console.log('Erro: ', error)
-      }
-  }
+    try {
+      await deletePessoa(id);
+    } catch (error) {
+      console.log('Erro: ', error);
+    }
+  };
 
+  if (!pessoasQuery) return <div>Loading...</div>;
 
   return (
-        <>
-          <table className="table">
-          <thead className="table-rows-header">
-            <tr>
-              <th className="table-header">ID</th>
-              <th className="table-header">Nome</th>
-              <th className="table-header">Login</th>
-              <th className="table-header">CPF</th>
-              <th className="table-header">N° de inscrição</th>
-              <th className="table-header">Empresa</th>
-              <th className="table-header">Ações</th>
+    <>
+      <table className="table">
+        <thead className="table-rows-header">
+          <tr>
+            <th className="table-header">ID</th>
+            <th className="table-header">Nome</th>
+            <th className="table-header">Login</th>
+            <th className="table-header">CPF</th>
+            <th className="table-header">N° de inscrição</th>
+            <th className="table-header">Empresa</th>
+            <th className="table-header">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {slice.map((el: Pessoa) => (
+            <tr className="table-rows-item" key={el.id}>
+              <td className="table-cell">{el.id}</td>
+              <td className="table-cell">{el.nome}</td>
+              <td className="table-cell">{el.login}</td>
+              <td className="table-cell">{el.cpf}</td>
+              <td className="table-cell">-</td>
+              <td className="table-cell">-</td>
+              <td className="table-cell" style={{ display: 'flex' }}>
+                <button onClick={() => UpdatePessoa(el.id)} style={{ backgroundColor: '#138dba'}} title='Editar Usuário'>
+                  <FontAwesomeIcon icon={faPen} />
+                </button>
+                <button onClick={() => DeletePessoa(el.id)} style={{ backgroundColor: '#ba132c'}} title='Excluir Usuário'>
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {slice.map((el) => (
-              <tr className="table-rows-item" key={el.id}>
-                <td className="table-cell">{el.id}</td>
-                <td className="table-cell">{el.nome}</td>
-                <td className="table-cell">{el.login}</td>
-                <td className="table-cell">{el.cpf}</td>
-                <td className="table-cell">-</td>
-                <td className="table-cell">-</td>
-                <td className="table-cell" style={{ display: 'flex' }}>
-                    <button onClick={() => UpdatePessoa(el.id)} style={{ backgroundColor: '#138dba'}} title='Editar Usuário'>
-                            <FontAwesomeIcon icon={faPen} />
-                    </button>
-                    <button onClick={() => DeletePessoa(el.id)} style={{ backgroundColor: '#ba132c'}} title='Excluir Usuário'>
-                        <FontAwesomeIcon icon={faTrash} />
-                    </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
-      </>
-    );
+          ))}
+        </tbody>
+      </table>
+      <TableFooter range={range} setPage={setPage} page={page} />
+    </>
+  );
 }
 
 export default TableUsers;
